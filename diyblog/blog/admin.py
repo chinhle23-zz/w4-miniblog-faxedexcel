0a95a81ca155aa5dbfcce301afd8d98a5bc82aca
faxedexcel <chinh.le23@gmail.com>
2019-03-10 04:52:13 -0400

    got home page up and running

--- diyblog/blog/admin.py
+++ diyblog/blog/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from blog.models import Blogger, Blog
 
 # Register your models here.
-admin.site.register(Blogger)
-admin.site.register(Blog)
+# admin.site.register(Blogger)
+# admin.site.register(Blog)
+
+@admin.register(Blogger) # decorator to register the models...does exact same this as 'admin.site.register(Blogger, BloggerAdmin)  
+class BloggerAdmin(admin.ModelAdmin):
+    list_display = ('username',)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'post_date')
+    list_filter = ('post_date', 'author')
--- diyblog/blog/views.py
+++ diyblog/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from blog.models import Blogger, Blog
+from django.views import generic
 
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_blogs = Blog.objects.all().count()
+    
+    # The 'all()' is implied by default.    
+    num_bloggers = Blogger.objects.count()
+
+    context = {
+        'num_blogs': num_blogs,
+        'num_bloggers': num_bloggers,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
